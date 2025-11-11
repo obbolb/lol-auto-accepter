0cde087cb2d55ae28e9bb1ab75eb014b690efcb2
nopphawan chotirat <74129032+obbolb@users.noreply.github.com.>
2025-11-12 01:17:13 +0900

    requesting help

--- lol_auto_acceptor/client_test.py
+++ lol_auto_acceptor/client_test.py
@@ -1,7 +1,13 @@
 from client.LeagueClient import LeagueClient
 import sys
 
+'''
+wanna test:
+/lol-champ-select/v1/session/my-selection -> หลังกดล้อคตัว
+/lol-champ-select/v1/session -> กดทั้งช่วงเเบน พิ้ค หลังกดตัว หลังช่วงเลือกรูน
 
+
+'''
 try:
     client = LeagueClient()
     client.connect()
